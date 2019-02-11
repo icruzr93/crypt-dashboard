5f69dfb8f9b9248d776e409278411a9a09a98343
Isaac <isaac@ktbo.mx>
2019-02-10 19:38:03 -0600

    Finished App

--- src/components/common/Search.js
+++ src/components/common/Search.js
@@ -3,36 +3,85 @@ import "./Search.css";
 import { handleResponse } from "../../helpers";
 import { API_URL } from "../../config";
 import Loading from "../common/Loading";
+import { withRouter } from "react-router-dom";
 
 class Search extends React.Component {
   state = {
+    searchResults: [],
     loading: false
   };
 
   constructor() {
     super();
     this.handleChange = this.handleChange.bind(this);
+    this.handleRedirect = this.handleRedirect.bind(this);
   }
 
   handleChange(e) {
-    const inputName = e.target.name;
-    const inputValue = e.target.value;
-    this.setState({ [inputName]: inputValue });
+    const searchQuery = e.target.value;
 
-    if (!inputValue) return "";
+    this.setState({ searchQuery });
+
+    if (!searchQuery) return "";
 
     this.setState({ loading: true });
 
-    fetch(`${API_URL}/autocomplete?searchQuery=${inputValue}`)
+    fetch(`${API_URL}/autocomplete?searchQuery=${searchQuery}`)
       .then(handleResponse)
       .then(result => {
-        this.setState({ loading: false });
-        console.log(result);
+        this.setState({
+          loading: false,
+          searchResults: result
+        });
       });
   }
 
+  handleRedirect(currencyId) {
+    // Clear input value and close autocomplete container.
+    // By clearing searchQuery state.
+    this.setState({
+      searchQuery: "",
+      searchResults: []
+    });
+
+    this.props.history.push(`/currency/${currencyId}`);
+  }
+
+  renderSearchResults() {
+    const { searchResults, searchQuery, loading } = this.state;
+
+    if (!searchQuery) {
+      return "";
+    }
+
+    if (searchResults.length > 0) {
+      return (
+        <div className="Search-result-container">
+          {searchResults.map(result => (
+            <div
+              key={result.id}
+              className="Search-result"
+              onClick={() => this.handleRedirect(result.id)}
+            >
+              {result.name} ({result.symbol})
+            </div>
+          ))}
+        </div>
+      );
+    }
+
+    if (!loading) {
+      return (
+        <div className="Search-result-container">
+          <div className="Search-no-result">No results found</div>
+        </div>
+      );
+    }
+  }
+
   render() {
-    const { loading } = this.state;
+    const { loading, searchQuery } = this.state;
+
     return (
       <div className="Search">
         <span className="Search-icon" />
@@ -42,6 +91,7 @@ class Search extends React.Component {
           placeholder="Currency Name"
           name="query"
           onChange={this.handleChange}
+          value={searchQuery}
         />
 
         {loading && (
@@ -49,9 +99,11 @@ class Search extends React.Component {
             <Loading width={"12px"} height={"12px"} />
           </div>
         )}
+
+        {this.renderSearchResults()}
       </div>
     );
   }
 }
 
-export default Search;
+export default withRouter(Search);
--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -13,6 +13,18 @@ class Detail extends React.Component {
 
   componentDidMount() {
     const currencyId = this.props.match.params.id;
+    this.fetchCurrency(currencyId);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.location.pathname !== nextProps.location.pathname) {
+      // Get the new currency Id
+      const newCurrenyId = nextProps.match.params.id;
+      this.fetchCurrency(newCurrenyId);
+    }
+  }
+
+  fetchCurrency(currencyId) {
     this.setState({ loading: true });
 
     fetch(`${API_URL}/cryptocurrencies/${currencyId}`)
@@ -30,6 +42,7 @@ class Detail extends React.Component {
         });
       });
   }
+
   render() {
     const { loading, error, currency } = this.state;
 
